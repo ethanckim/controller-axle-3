71fee9abf40300b6a1a1c78c4648938febedefbb
IshaanGupta14 <71522054+IshaanGupta14@users.noreply.github.com>
2020-09-18 13:59:05 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+input.onLoudSound(function () {
+    light.showAnimation(light.cometAnimation, 1000)
+})
 input.onGesture(Gesture.Shake, function () {
     music.baDing.play()
     light.showAnimation(light.rainbowAnimation, 500)
@@ -5,17 +8,17 @@ input.onGesture(Gesture.Shake, function () {
 forever(function () {
     if (input.buttonA.isPressed()) {
         light.showRing(
-        `red red red red red black black black black black`
+        "red red red red red black black black black black"
         )
         makerController.player1.setButton(ArcadeButton.Left, true)
     } else if (input.buttonB.isPressed()) {
         light.showRing(
-        `black black black black black red red red red red`
+        "black black black black black red red red red red"
         )
         makerController.player1.setButton(ArcadeButton.Right, true)
     } else if (!(input.buttonA.isPressed()) && !(input.buttonB.isPressed())) {
         light.showRing(
-        `black black black black black black black black black black`
+        "black black black black black black black black black black"
         )
         makerController.player1.setButton(ArcadeButton.Left, false)
         makerController.player1.setButton(ArcadeButton.Right, false)
