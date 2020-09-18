a857b170a34533a03886b000edf8844fcb1c1580
Ethan Calvin Kim <robhn38426@gmail.com>
2020-09-18 10:30:45 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+forever(function () {
+    if (input.buttonA.isPressed()) {
+        light.showRing(
+        `red red red red red black black black black black`
+        )
+        makerController.player1.setButton(ArcadeButton.Left, true)
+    } else if (input.buttonB.isPressed()) {
+        light.showRing(
+        `black black black black black red red red red red`
+        )
+        makerController.player1.setButton(ArcadeButton.Right, true)
+    } else if (!(input.buttonA.isPressed()) && !(input.buttonB.isPressed())) {
+        light.showRing(
+        `black black black black black black black black black black`
+        )
+        makerController.player1.setButton(ArcadeButton.Left, false)
+        makerController.player1.setButton(ArcadeButton.Right, false)
+    }
+})
